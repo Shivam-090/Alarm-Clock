2e3ca07bcbc83a4aecf33095ea1e20a596c547df
SHIVAM <93383164+Shivam-090@users.noreply.github.com>
2022-11-15 13:37:28 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from tkinter import *
+import datetime as dt
+from time import strftime
+from stopwatch import *
+from alarm import *
+from timer import *
+
+root = Tk()
+root.title("Digital Clock Project")
+root.geometry("850x550")
+
+heading = Label(root, text="Digital Clock", font=(
+    "Helvetica 25 bold"), fg="red").pack(pady=30)
+
+date = dt.datetime.now()
+frame = Frame(root)
+frame.pack()
+bottomframe = Frame(root)
+bottomframe.pack(side=BOTTOM)
+leftframe = Frame(root)
+leftframe.pack(side=LEFT)
+rightframe = Frame(root)
+rightframe.pack(side=RIGHT)
+
+
+def timing():
+    string = strftime('%H:%M:%S %p')
+    real_time1.config(text=string)
+    real_time1.after(1000, timing)
+
+
+# button_dict = {}
+# option = ["Alarm", "Stopwatch", "Timer"]
+
+# for i in option:
+#     def test(x = i):
+#         if x == "Alarm":
+#             alarm_clock()
+#         elif x == "Stopwatch":
+#             stopwatch()
+#         elif x == "Timer":
+#             timer()
+
+#     button_dict[i] = Button(root, text=i, background="white", activebackground="light cyan", padx=70, pady=10, width=5,command=test).pack(pady=25, ipadx=25)
+    
+alarm_btn = Button(leftframe, text="Alarm", background="white", activebackground="light cyan", padx=70, pady=10, width=5, command= alarm_clock).pack()
+# stopwatch_btn = Button(leftframe, text="Stopwatch", background="white", activebackground="light cyan", padx=70, pady=10, width=5, command= stopwatch).pack()
+timer_btn = Button(leftframe, text="Timer", background="white", activebackground="light cyan", padx=70, pady=10, width=5, command= timer).pack()
+real_time = Label(
+    rightframe, text=f"{date:%A, %B %d, %Y}", font="Calibri, 20").pack(padx=40)
+real_time1 = Label(rightframe, font=("Calibri 20"))
+real_time1.pack()
+timing()
+btn = Button(bottomframe, text='Exit!', command=root.destroy,
+             width=10, activebackground="#ff033e", anchor="n").pack(pady=60)
+root.mainloop()
---
+++ stopwatch.py
@@ -0,0 +1,67 @@
+from tkinter import *
+from tkinter.ttk import *
+from datetime import datetime
+
+counter = 66600
+running = False
+def counter_label(label):
+    def count():
+        if running:
+            global counter
+    # To manage the initial delay.
+            if counter == 66600:
+                display = "Starting..."
+            else:
+                tt = datetime.fromtimestamp(counter)
+                string = tt.strftime("%H:%M:%S")
+                display = string
+            label['text'] = display
+            label.after(1000, count)
+            counter += 1
+    # Triggering the start of the counter.
+    count()
+# start function of the stopwatch
+def Start(label):
+    global running
+    running = True
+    counter_label(label)
+    start['state'] = 'disabled'
+    stop['state'] = 'normal'
+    reset['state'] = 'normal'
+# Stop function of the stopwatch
+def Stop():
+    global running
+    start['state'] = 'normal'
+    stop['state'] = 'disabled'
+    reset['state'] = 'normal'
+    running = False
+# Reset function of the stopwatch
+def Reset(label):
+    global counter
+    counter = 66600
+    # If rest is pressed after pressing stop.
+    if running == False:
+        reset['state'] = 'disabled'
+        label['text'] = 'Welcome!'
+    # If reset is pressed while the stopwatch is running.
+    else:
+        label['text'] = 'Starting...'
+stpwtch_root = Tk()
+stpwtch_root.title("Stopwatch")
+# stpwtch_root.iconbitmap("stopwatch.ico")
+stpwtch_root.configure()
+# Fixing the window size.
+stpwtch_root.minsize(width=250, height=70)
+label = Label(stpwtch_root, text="Welcome!", font=("Arial", 25))
+# label = Label(stpwtch_root, text="Welcome!", fg="#4B8BBE", bg="#FFE873", font="Verdana 30 bold")
+label.pack()
+f = Frame(stpwtch_root)
+start = Button(f, text='Start', width=6, command=lambda: Start(label))
+stop = Button(f, text='Stop', width=6, state='disabled', command=Stop)
+reset = Button(f, text='Reset', width=6,
+                    state='disabled', command=lambda: Reset(label))
+f.pack(anchor='center', pady=5)
+start.pack(fill = BOTH , side="left")
+stop.pack(fill = BOTH , side="left")
+reset.pack(fill = BOTH ,side="left")
+stpwtch_root.mainloop()
